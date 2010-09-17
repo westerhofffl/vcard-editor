43d9e26f29c4adcb91a95f76354a5f38fdebf870
EllekeBindi <EllekeBindi@7fea344d-d123-807c-3f15-78813f88adef>
2010-09-17 16:49:51 +0000

    * plastiquestyle

--- dude/main.cpp
+++ dude/main.cpp
@@ -1,9 +1,11 @@
 #include <QtGui/QApplication>
 #include "MainWindow.h"
+#include <QPlastiqueStyle>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setStyle(new QPlastiqueStyle);
     MainWindow w;
     w.show();
 
